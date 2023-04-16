4900fc49a73b27b44499551ea2f3cfe94a15dcad
jj <jacqulinenyokabi@gmail.com>
2023-04-16 18:27:15 +0300

    Deploy branch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
+import {useEffect} from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {ToastContainer} from "react-toastify";
 import ContextProvider from "./Context/ContextProvider";
 import About from "./Pages/About/About";
 import Contact from "./Pages/Contact/Contact";
@@ -11,39 +11,39 @@ import Portfolio from "./Pages/Portfilo/Portfolio";
 import Resume from "./Pages/Resume/Resume";
 
 function App() {
-  useEffect(() => {
-    AOS.init({ duration: 1200 });
-    AOS.refresh();
-  }, []);
-  return (
-    <>
-      <ContextProvider>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
-          <Routes>
-            <Route path="/" element={<Home />}>
-              <Route index element={<About />} />
-              <Route path="about" element={<About />} />
-              <Route path="resume" element={<Resume />} />
-              <Route path="contact" element={<Contact />} />
-              <Route path="works" element={<Portfolio />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </ContextProvider>
+    useEffect(() => {
+        AOS.init({duration: 1200});
+        AOS.refresh();
+    }, []);
+    return (
+        <>
+            <ContextProvider>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Home/>}>
+                            <Route index element={<About/>}/>
+                            <Route path="about" element={<About/>}/>
+                            <Route path="resume" element={<Resume/>}/>
+                            <Route path="contact" element={<Contact/>}/>
+                            <Route path="works" element={<Portfolio/>}/>
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
+            </ContextProvider>
 
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-    </>
-  );
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
+        </>
+    );
 }
 
 export default App;
